629b2607830dff3641add58e8339f88c69905b7c
milkteahot <greenegoode11@gmail.com>
2020-06-23 21:34:56 +0900

    Create product.js

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,14 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //=================================
 //             Product
 //=================================
 
-router.post('/image', (req, res) => {
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  }
+});
 
-    //가져온 이미지를 저장
-    
-})
+var upload = multer({ storage: storage });
+
+router.post("/image", (req, res) => {
+  //가져온 이미지를 저장
+  upload(req, res, err => {
+      if(err) {
+          return req.json({ sucess: false, err })
+      } 
+      return res.json({ sucess: this.true, filePath: res.req.file.path , fileName: res.req.file.filename })
+  })
+});
 
 module.exports = router;
