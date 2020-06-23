924fe7594793609fcdcf88222749d68d875fbbc9
milkteahot <greenegoode11@gmail.com>
2020-06-23 21:28:53 +0900

    Create FileUpload

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import Dropzone from "react-dropzone";
+import { Icon } from "antd";
+import axios from "axios";
 
 function FileUpload() {
-    return (
-        <div>
-            upload
-        </div>
-    )
+  const dropHandler = files => {
+    let formData = new FormData();
+
+    const config = {
+      header: { "content-type": "multipart/form-data" }
+    };
+    formData.append("file", files[0]);
+
+    axios.post("/api/product/image", formData, config)
+        .then(response => {
+            if(response.data.success) {
+
+            } else {
+                alert('파일 저장에 실패했습니다.')
+            }
+        })
+  };
+
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <Dropzone onDrop={dropHandler}>
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div
+              style={{
+                width: 300,
+                height: 240,
+                border: "1px solid lightgray",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+              {...getRootProps()}
+            >
+              <input {...getInputProps()} />
+              <Icon type="plus" style={{ fontSize: "3rem" }} />
+            </div>
+          </section>
+        )}
+      </Dropzone>
+    </div>
+  );
 }
 
-export default FileUpload
+export default FileUpload;
---
+++ server/routes/product.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+//=================================
+//             Product
+//=================================
+
+router.post('/image', (req, res) => {
+
+    //가져온 이미지를 저장
+    
+})
+
+module.exports = router;
